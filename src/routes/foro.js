80dba0cdc40363e320e935e9d14c376d8a0b29d7
Dylan <gabomojica69@gmail.com>
2024-11-17 15:21:37 -0500

    PUT
    Para actualizar el foro

--- src/routes/foro.js
+++ src/routes/foro.js
@@ -33,3 +33,22 @@ router.get("/foros/:id", (req, res) => {
         })
         .catch((error) => res.status(500).json({ message: error.message }));
 });
+
+router.put("/foros/:id", async (req, res) => {
+    const { id } = req.params;
+    if (req.body.musica) {
+        const musicaConsulta = await musicaSchema.findOne({ titulo: req.body.musica.titulo });
+        if (!musicaConsulta) {
+            const nuevaMusica = new musicaSchema(req.body.musica);
+            await nuevaMusica.save().then((dataMusica) => {
+                req.body.musica = dataMusica._id;
+            }).catch((error) => res.status(400).json({ message: error.message }));
+        } else {
+            req.body.musica = musicaConsulta._id;
+        }
+    }
+    foroSchema
+        .updateOne({ _id: id }, { $set: req.body })
+        .then((data) => res.json(data))
+        .catch((error) => res.status(400).json({ message: error.message }));
+});
