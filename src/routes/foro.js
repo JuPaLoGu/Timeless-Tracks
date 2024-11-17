4b87bdbfff53a819c45e12c080d07e232b74c1b1
Dylan <gabomojica69@gmail.com>
2024-11-17 16:17:15 -0500

    Eliminación import innecesario

--- src/routes/foro.js
+++ src/routes/foro.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const foroSchema = require("../models/foro");
-const userSchema = require("../models/user");
 const musicaSchema = require("../models/musica");
 
 router.post("/foros", (req, res) => {
