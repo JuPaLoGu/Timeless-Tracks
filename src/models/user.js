0d1184da9acf519f1b530cd774f1c23cbfb8496e
ZAXTER <juanpablo112000@yahoo.com.co>
2024-11-17 21:15:01 -0500

    Últimos comentarios
    Se agregaron los últimos comentarios, en el archivo user.js para un mejor manejo del mismo.

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,27 @@
-const mongoose = require("mongoose"); // importando el componente mongoose
-const bcrypt = require("bcrypt"); // importando el componente bcrypt
+// const mongoose = require("mongoose"); // Importa Mongoose para trabajar con MongoDB.
+const bcrypt = require("bcrypt"); // Importa bcrypt para manejar el hash de contraseñas.
+
+// Definición del esquema para la colección "User".
 const userSchema = mongoose.Schema({
     usuario: {
-        type: String,
-        required: true
+        type: String, // Nombre del usuario, tipo String.
+        required: true // Campo obligatorio.
     },
     correo: {
-        type: String,
-        required: true
+        type: String, // Dirección de correo electrónico, tipo String.
+        required: true // Campo obligatorio.
     },
     clave: {
-        type: String,
-        required: true
+        type: String, // Contraseña del usuario, tipo String.
+        required: true // Campo obligatorio.
     }
 });
+
+// Método para encriptar la contraseña antes de almacenarla.
 userSchema.methods.encryptClave = async (clave) => {
-    const salt = await bcrypt.genSalt(10);
-    return bcrypt.hash(clave, salt);
+    const salt = await bcrypt.genSalt(10); // Genera un salt con un factor de complejidad de 10.
+    return bcrypt.hash(clave, salt); // Crea un hash de la contraseña utilizando el salt.
 }
+
+// Exporta el modelo "User" basado en el esquema definido.
 module.exports = mongoose.model('User', userSchema);
